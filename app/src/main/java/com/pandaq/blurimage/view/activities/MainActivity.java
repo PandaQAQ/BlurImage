294f9ebb35b7bb82cdd0a65a6bbfa49b552efc50
huxinyu <767807368@qq.com>
2016-09-12 09:21:30 +0800

    add readme.md

--- app/src/main/java/com/pandaq/blurimage/view/activities/MainActivity.java
+++ app/src/main/java/com/pandaq/blurimage/view/activities/MainActivity.java
@@ -8,6 +8,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 
 import com.pandaq.blurimage.R;
@@ -42,20 +43,21 @@ public class MainActivity extends Activity implements SeekBar.OnSeekBarChangeLis
         ButterKnife.bind(this);
         mBlur.setOnSeekBarChangeListener(this);
         mSampling.setOnSeekBarChangeListener(this);
-        image_bitmap = ((BitmapDrawable) mImage.getDrawable()).getBitmap();
-        bitmap_width = image_bitmap.getWidth();
-        bitmap_hight = image_bitmap.getHeight();
-        initBackGround();
+//        image_bitmap = ((BitmapDrawable) mImage.getDrawable()).getBitmap();
+        initBackGround(mImage);
     }
 
-    private void initBackGround() {
+    private void initBackGround(ImageView image) {
+        Bitmap bitmap = ((BitmapDrawable) image.getDrawable()).getBitmap();
         //创建一个长宽等比缩小的Bitmap
+        bitmap_width = bitmap.getWidth();
+        bitmap_hight = bitmap.getHeight();
         Bitmap overlay = Bitmap.createBitmap((int) (bitmap_width / scaleFactor), (int) (bitmap_hight / scaleFactor), Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(overlay);
         //将canvas按照bitmap等量缩放，模糊处理的图片才能显示正常
         canvas.scale(1 / scaleFactor, 1 / scaleFactor);
         Paint paint = new Paint(Paint.FILTER_BITMAP_FLAG);
-        canvas.drawBitmap(image_bitmap, 0, 0, paint);
+        canvas.drawBitmap(bitmap, 0, 0, paint);
         //对采样后的bitmap进行模糊处理，缩放采样后的图片处理比原图处理要省很多时间和内存开销
         overlay = FastBlur.doBlur(overlay, (int) blur, false);
         //模糊处理后的图片设置为头部布局背景图
@@ -71,11 +73,11 @@ public class MainActivity extends Activity implements SeekBar.OnSeekBarChangeLis
         switch (seekBar.getId()) {
             case R.id.sampling:
                 scaleFactor = i;
-                initBackGround();
+                initBackGround(mImage);
                 break;
             case R.id.blur:
                 blur = i;
-                initBackGround();
+                initBackGround(mImage);
                 break;
         }
     }
